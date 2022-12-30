598b26e5b59e8ac0aa696382341e9d3b1113e0b9
Alphago5872 <alpha.kittikhom@gmail.com>
2022-12-30 11:38:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [button, setButton] = useState(false);
+
+  async function fetchStates() {
+    const response = await fetch(
+      "https://arduino-shitshow-default-rtdb.asia-southeast1.firebasedatabase.app/.json"
+    );
+    const data = await response.json();
+
+    console.log(data);
+
+    setButton(data.button);
+  }
+
+  useEffect(() => {
+    fetchStates();
+  }, []);
+
+  async function pushStates() {
+    const response = await fetch('https://arduino-shitshow-default-rtdb.asia-southeast1.firebasedatabase.app/.json', {
+      method: 'PATCH',
+      body: JSON.stringify({button: button}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json();
+
+  }
+
+  useEffect(() => {
+    pushStates()
+  }, [button])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Arduino controller :)</h1>
+      <h2>Controller LED</h2>
+      <h3>
+        Current state {"->"} {button ? "on" : "off"}
+      </h3>
+      <button
+        onClick={() => {
+          setButton(!button);
+        }}
+      >
+        TOGGLE BUTTON
+      </button>
     </div>
   );
 }
